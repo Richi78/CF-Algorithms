f595acb813da37c3cda7670d77a66f054e1dcc1c
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-15 12:11:39 -0400

    add: 15-05

---
+++ C_Splitting_Items.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define all(v) v.begin(),v.end()
+#define vii vector<pair<int,int>>
+#define F first 
+#define S second
+#define pii pair<int,int>
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    int n,k; cin >> n >> k;
+    vector<int> a(n);
+    for(int i=0 ; i<n ; i++) cin >> a[i];
+    sort(all(a));
+    int mx=a.back() , cnt=0;
+    bool turn=1; // alice -> 1 
+    int ans=0;
+    for(int i=n-1 ; i>=0 ; i--){
+        if(a[i] == mx){
+            cnt++;
+        }else{
+            if(cnt % 2 == 0){
+                mx=a[i]; cnt=1;
+            }else{
+                int tmp=mx-a[i];
+                if(k > tmp){
+                    a[i]=mx;
+                    k-=tmp;
+                    cnt++;
+                }else{
+                    a[i]+=k;
+                    k=0;
+                }
+            }
+        }
+
+        if(turn) ans+=a[i];
+        else ans-=a[i];
+        turn=!turn;
+    }
+    cout<<ans<<endl;
+}
+
+signed main(){
+    FIO;
+    int tc;cin>>tc;
+    while(tc--)solve();
+}
