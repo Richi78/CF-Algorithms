4a4e874ab9cf47c44d7d713f3d6c6ab6250c00c9
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-19 19:13:02 -0400

    add: 19-05 daily practice

---
+++ B_Maximize_Mex.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define vii vector<pair<int,int>>
+#define F first 
+#define S second
+#define pii pair<int,int>
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    int n,k; cin >> n >> k;
+    vector<int> a(n);
+    for(int i=0 ; i<n ; i++){
+        int x; cin >> x;
+        if(x < n) a[x]++;
+    }
+
+    for(int i=0 ; i<n ; i++){
+        if(a[i]==0){cout<<i<<endl; return;}
+        if(a[i]>1){
+            if(i+k < n)a[i+k]+=a[i]-1;
+            a[i]=1;
+        }
+    }
+    cout<<n<<endl;
+}
+
+signed main(){
+    FIO;
+    int tc;cin>>tc;
+    while(tc--)solve();
+}
+
