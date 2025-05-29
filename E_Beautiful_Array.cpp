38ed8cde35423fd5b4ce63f1703435030d747fb2
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-28 23:54:12 -0400

    add: 28-05 daily practice

---
+++ E_Beautiful_Array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define F first 
+#define S second
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    int a,b; cin >> a >> b;
+
+    int tmp=a*3-2*b;
+
+    cout<<"3\n";
+    cout<<b<<" "<<b<<" "<<tmp<<endl;
+}
+
+signed main(){
+    FIO;
+    // int tc;cin>>tc;
+    // while(tc--)solve();
+    solve();
+}
