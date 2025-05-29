6db5b8c34ca6dd299a73e80fb91f1b7da4c31423
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-28 22:42:56 -0400

    add: 28-05 daily practice

---
+++ D_Harder_Problem.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define F first 
+#define S second
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    int n; cin >> n;
+    vector<int> a(n), b(n+1);
+    map<int,int> mp;
+    for(int i=0 ; i<n ; i++){
+        cin >> a[i];
+        b[a[i]]=1;
+    }
+    int next=1;
+    for(int i=0 ; i<n ; i++){
+        int x=a[i];
+        if(mp.count(x) == 1){
+            while(mp[next]==1)next++;
+            mp[next]++;
+            cout<<next<<" ";
+            next++;
+        }else{
+            cout<<x<<" ";
+            mp[x]++;
+        }
+    }
+    cout<<endl;
+}
+
+signed main(){
+    FIO;
+    int tc;cin>>tc;
+    while(tc--)solve();
+}
