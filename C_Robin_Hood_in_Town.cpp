9ba7c6e7cbabd961a217645b78a2a59181446cd4
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-29 22:48:55 -0400

    add: 29-05 daily practice

--- C_Robin_Hood_in_Town.cpp
+++ C_Robin_Hood_in_Town.cpp
@@ -6,41 +6,27 @@ using namespace std;
 #define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
 #define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
 #define int long long
-#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define all(v) v.begin(),v.end()
-#define vii vector<pair<int,int>>
 #define F first 
 #define S second
-#define pii pair<int,int>
 
 template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
 template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
 
 void solve(){
-    int n,m,a,b; cin >> n >> m >> a >> b;
-    
-    vector< pair<int,int> > dim={
-        {a,m} , {n-a+1,m} , {n,b} , {n,m-b+1}
-    };
-
-    int ans=1e9;
-    for(auto [x,y] : dim){
-        int tmp=0;
-        while(x>1){
-            tmp++; x=(x+1)/2;
-        }
-        while(y>1){
-            tmp++; y=(y+1)/2;
-        }
-        ans=min(ans,tmp);
-    }
-    cout<< 1+ans <<endl;
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int i=0 ; i<n ; i++) cin >> a[i];
+    if(n==1 || n ==2){cout<<-1<<endl; return;}
+    sort(all(a));
+    int sum=accumulate(all(a),0LL);
+    int x = 2 * n * a[n/2] - sum + 1;
+    cout<< max(0LL,x) <<endl;
 }
 
 signed main(){
-    // FIO;
+    FIO;
     int tc;cin>>tc;
     while(tc--)solve();
 }
-
-
