aabfb29b9fa0e5d89f264317a9fa764aaf440939
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-30 23:48:01 -0400

    add: 30-05 daily practice

--- x_BB.cpp
+++ x_BB.cpp
@@ -2,25 +2,24 @@
 
 using namespace std;
 
-int main()
-{
-    string a,b;
-    while(getline(cin,a)){
-        getline(cin,b);
-        int aCount[26] = {};
-        int bCount[26] = {};
-        for(auto& c : a){
-            aCount[c-'a']++;
-        }
-        for(auto& c : b){
-            bCount[c-'a']++;
-        }
-        string res = "";
-        for(int i=0;i<26;i++){
-            int common = min(aCount[i],bCount[i]);
-            for(int j=0;j<common;j++)
-                res += (char)(i+'a');
-        }
-        cout << res << endl;
-    }
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define F first 
+#define S second
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    
+}
+
+signed main(){
+    FIO;
+    int tc;cin>>tc;
+    while(tc--)solve();
 }
