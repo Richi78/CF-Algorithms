1ca51486c755cc80d4702b6e9472518251462ccb
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-03 00:52:12 -0400

    add: 02-06 daily practice

---
+++ B_Ten_Words_of_Wisdom.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define F first 
+#define S second
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    int n; cin >> n;
+    int mx=-1 , idx=-1;
+    for(int i=1 ; i<=n ; i++){
+        int x,y; cin >> x >> y;
+        if(x>10) continue;
+        if(uax(mx,y)) idx=i;
+    }
+    cout<<idx<<endl;
+}
+
+signed main(){
+    FIO;
+    int tc;cin>>tc;
+    while(tc--)solve();
+}
