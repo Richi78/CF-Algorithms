93a47ed37634ccf702c4705fe9a6bae700a2a8f0
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-13 01:16:27 -0400

    add: geometry cses

--- CSES_Convex_hull.cpp
+++ CSES_Convex_hull.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define debug1(x) cout << #x << " = " << x << "\n";
 #define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
 #define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
-// #define int long long
+#define int long long
 #define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define all(v) v.begin(),v.end()
 #define F first 
@@ -17,76 +17,56 @@ template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
 template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
 
 struct Point{
-    int x , y;
-    Point (int x, int y): x(x) , y(y){}
+    int x,y;
+    void read(){cin >> x >> y;}
+    Point operator -(const Point &b) const {
+        return Point{x-b.x , y-b.y};
+    }
+    int operator *(const Point &b) const {
+        return x*b.y - y*b.x; 
+    }
+    int cross(const Point &b, const Point &c) const {
+        return (b-*this) * (c-*this);
+    }
+    bool operator <(const Point &b) const {
+        return make_pair(x,y)<make_pair(b.x,b.y);
+    }
 };
-
-
-int orientation(Point a, Point b, Point c){
-    long long v = 1LL*a.x*(b.y-c.y)
-                + 1LL*b.x*(c.y-a.y)
-                + 1LL*c.x*(a.y-b.y);
-    if(v < 0) return -1;
-    if(v > 0) return +1;
-    return 0;
-}
-
-bool cw(Point a, Point b, Point c, bool include_collinear){
-    int o=orientation(a,b,c);
-    return o < 0 || (include_collinear && o==0);
-}
-
-bool collinear(Point a, Point b, Point c) {
-    return orientation(a,b,c)==0;
-}
-
-void convex_hull(vector<Point> &a, bool include_collinear){
-    Point p0 = *min_element(a.begin(), a.end(), [](Point a, Point b){
-        return make_pair(a.y,a.x) < make_pair(b.y,b.x);
-    });
-    sort(a.begin(), a.end(), [&p0](const Point &a, const Point &b){
-        int o = orientation(p0,a,b);
-        if(o==0) 
-            return (p0.x-a.x)*(p0.x-a.x) + (p0.y-a.y)*(p0.y-a.y)
-                < (p0.x-b.x)*(p0.x-b.x) + (p0.y-b.y)*(p0.y-b.y); 
-        return o<0;
-    });
-    if(include_collinear){
-        int i = a.size()-1;
-        while(i >= 0 && collinear(p0,a[i], a.back())) i--;
-        reverse(a.begin()+i+1, a.end());
-    }
-
-    vector<Point> st;
-    for(int i=0 ; i<a.size() ; i++){
-        while(st.size()>1 && !cw(st[st.size()-2], st.back(), a[i], include_collinear))
-            st.pop_back();
-        st.push_back(a[i]);
-    }
-
-    a=st;
-}
 
 void solve(){
     int n; cin >> n;
-    vector<Point> points;
-    for(int i=0 ; i<n ; i++){
-        int x,y; cin >> x >> y;
-        points.emplace_back(x,y);
+    vector<Point> points(n);
+    for(auto &x : points) x.read();
+
+    sort(all(points));
+
+    vector<Point> convex;
+
+    for(int i=0 ; i<2 ; i++){
+        const int S=convex.size();
+        for(Point c : points){
+            while((int)convex.size() >= S+2){
+                Point a=convex.end()[-2] , b=convex.end()[-1];
+                // check si esta a la izq
+                if( a.cross(b,c) <= 0 ){ // <= para colinear
+                    break;
+                }
+                convex.pop_back();
+            }
+            convex.push_back(c);
+        }
+        convex.pop_back();
+        reverse(all(points));
     }
-
-    convex_hull(points , true);
-
-    cout<<points.size()<<endl;
-    for(int i=0 ; i<points.size() ; i++){
-        cout<<points[i].x<< " " <<points[i].y<<endl;
+    cout<<(int)convex.size()<<"\n";
+    for(auto c : convex){
+        cout<< c.x << " " << c.y << "\n";
     }
 }
 
-int main(){
+signed main(){
     FIO;
     // int tc;cin>>tc;
     // while(tc--)solve();
     solve();
-    return 0;
 }
