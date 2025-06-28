4cfc85e624011c5dacfa5b83f1a59001df0f189e
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-28 00:03:08 -0400

    add: 27-06 daily practice

---
+++ B_ICPC_Balloons.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define F first 
+#define S second
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    int n; cin >> n;
+    string s; cin >> s;
+    vector<int> a(26);
+    int ans=0;
+    for(int i=0 ; i<n ; i++){
+        if(a[s[i]-'A']){
+            ans++;
+            a[s[i]-'A']++;
+        }else{
+            ans+=2;
+            a[s[i]-'A']+=2;
+        }
+    }
+    cout<< ans <<"\n";
+}
+
+signed main(){
+    FIO;
+    int tc;cin>>tc;
+    while(tc--)solve();
+}
