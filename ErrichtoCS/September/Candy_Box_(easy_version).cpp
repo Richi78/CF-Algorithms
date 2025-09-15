f47ba076b2b6eeead413ae8298a9fcbd3499ba35
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-09-14 23:30:00 -0400

    add: 14-09 daily practice

---
+++ ErrichtoCS/September/Candy_Box_(easy_version).cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+// const int MOD=998244353;
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    int n; cin >> n;
+    vector<int> freq(n+1);
+    for(int i=0 ; i<n ; i++){
+        int x; cin >> x;
+        freq[x]++;
+    }
+    sort(all(freq));
+    int ans=freq[n] , cur=freq[n];
+    for(int i=n-1 ; i>=0 ; i--){
+        if( freq[i] >= cur ){
+            ans+=--cur;
+        }else{
+            ans+=freq[i];
+            cur=freq[i];
+        }
+        if(cur == 0) break;
+    }
+    cout<< ans <<"\n";
+}
+
+signed main(){
+    FIO;
+    // freopen("censor.in", "r", stdin);
+    // freopen("censor.out", "w", stdout);
+    int tc;cin>>tc;
+    while(tc--)solve();
+}
