0212eb5a628f1343ac632a2a798e37c75fd752d9
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-10-24 15:44:31 -0400

    add: 24-10 daily practice

--- C_1_No_Cost_Too_Great_Easy_Version.cpp
+++ C_1_No_Cost_Too_Great_Easy_Version.cpp
@@ -13,28 +13,38 @@ using namespace std;
 template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
 template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
 
+const int N=2e5+5;
+vector<int> PF[N];
+
 void solve(){
     int n; cin >> n;
     vector<int> a(n),b(n);
-    map<int,int> mp;
-    int cnt_e=0 , cnt_o=0;
-    for(int i=0 ; i<n ; i++){
-        cin >> a[i];
-        mp[a[i]]++;
-        if(a[i]&1) cnt_o++;
-        else cnt_e++;
-    }
+    for(int i=0 ; i<n ; i++) cin >> a[i];
     for(int i=0 ; i<n ; i++) cin >> b[i];    
-
-    int ans=1e9;
-    if(cnt_e == 0) ans=min(ans,2LL);
-    else if(cnt_e == 1) ans=min(ans,1LL);
-    else if(cnt_e >= 2){
-        cout<< "0\n"; return;
-    }
+    
+    int ans=2;
+    map<int,int> mp;
     for(int i=0 ; i<n ; i++){
-        // it won't work this way
+        for(auto x : PF[a[i]]){
+            if(mp[x] > 0) ans=0;
+            mp[x]++;
+        }
     }
+
+    for(int i=0 ; i<n ; i++){
+        for(auto x : PF[a[i]]){
+            mp[x]--;
+        }
+        for(auto x : PF[a[i]+1]){
+            if(mp[x] > 0){
+                ans=min(ans,1LL);
+            }
+        }
+        for(auto x : PF[a[i]]){
+            mp[x]++;
+        }
+    }
+
     cout<< ans <<"\n";
 }
 
@@ -42,6 +52,19 @@ signed main(){
     FIO;
     // freopen("censor.in", "r", stdin);
     // freopen("censor.out", "w", stdout);
+    
+    vector<bool> crib(N,true);
+    crib[0]=crib[1]=false;
+    for(int i=2 ; i<N ; i++){
+        if(crib[i]){
+            for(int j=i ; j<N ; j+=i){
+                PF[j].push_back(i);
+                crib[j]=false;
+            }
+        }
+    }
+
+
     int tc;cin>>tc;
     while(tc--)solve();
 }
