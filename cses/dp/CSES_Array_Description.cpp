171ce097949b468d44dfafa2ec83c636de9df0ee
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-11-04 00:34:12 -0400

    add: 04-11 daily practice

--- cses/dp/CSES_Array_Description.cpp
+++ cses/dp/CSES_Array_Description.cpp
@@ -13,13 +13,37 @@ using namespace std;
 template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
 template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
 
+const int MOD=1e9+7;
+
 void solve(){
     int n,m; cin >> n >> m;
     vector<int> a(n);
     for(int i=0 ; i<n ; i++) cin >> a[i];
     
-    vector<vector<int>> dp(n+?????,vector<int>(m+?????));
-    for(int i)
+    vector<vector<int>> dp(n,vector<int>(m+1));
+    
+    if(a[0] == 0){
+        for(int i=0 ; i<=m ; i++) dp[0][i]=1;
+    }else{
+        dp[0][a[0]]=1;
+    }
+
+    for(int i=0 ; i<=n-2 ; i++){
+        for(int last=1 ; last<=m ; last++){
+            for(int y : {last-1,last,last+1}){
+                if(1<=y && y<=m){
+                    // if not valid continue
+                    if(a[i+1] != 0 && a[i+1] != y) continue;
+                    dp[i+1][y]=(dp[i+1][y]+dp[i][last])%MOD;
+                }
+            }
+        }
+    }
+    int ans=0;
+    for(int i=1 ; i<=m ; i++){
+        ans=(ans+dp[n-1][i])%MOD;
+    }
+    cout<< ans <<"\n";
 }
 
 signed main(){
