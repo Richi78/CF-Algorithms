a8c64bf4c5bcb1bcc708b8f5e0e4dc7abfb0be75
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-11-25 15:01:09 -0400

    add: 24-11 daily practice

---
+++ cses/dp/CSES_Money_Sums.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug1(x) cout << #x << " = " << x << "\n";
+#define debug2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define int long long
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+// const int MOD=998244353;
+
+template<typename T> bool uin(T &a, T b) {return a>b?(a=b,true):false;}
+template<typename T> bool uax(T &a, T b) {return a<b?(a=b,true):false;}
+
+void solve(){
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int i=0 ; i<n ; i++) cin >> a[i];
+
+    const int N=1e5+10;
+    vector<bool> dp(N);
+    dp[0]=true;
+    for(int i=0 ; i<n ; i++){
+        for(int j=N-1 ; j>=0 ; j--){
+            if(dp[j]) dp[j+a[i]]=true;
+        } 
+    }
+
+    vector<int> ans;
+    for(int i=1 ; i<N ; i++){
+        if(dp[i]) ans.push_back(i);
+    }
+    cout<< ans.size() <<"\n";
+    for(auto x : ans) cout<< x <<" ";
+    cout<<"\n";
+}
+
+signed main(){
+    FIO;
+    // freopen("censor.in", "r", stdin);
+    // freopen("censor.out", "w", stdout);
+    // int tc;cin>>tc;
+    // while(tc--)solve();
+    solve();
+}
