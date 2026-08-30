c3da9cb162126640e6d9fa916c1b2ebe959db8fc
Richi78 <ricardo.rojas.carvajal@gmail.com>
2026-08-30 10:16:31 -0400

    add: 30-08 daily practice

---
+++ M_Microondas.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long long INF=1e17;
+
+int main(){
+    int n,m,k; cin >> n >> m >> k;
+    vector<array<long long,3>> adj[n];
+    for(int i=0 ; i<m ; i++){
+        long long u,v,w,p; cin >> u >> v >> w >> p;
+        u--; v--;
+        adj[u].push_back({v,w,p});
+        adj[v].push_back({u,w,p});
+    }
+
+    vector<vector<long long>> dist(n,vector<long long>(k+1,INF));
+    for(int i=0 ; i<=k ; i++) dist[0][i] = 0LL;
+
+    set<array<long long,3>> st;
+    st.insert({0LL, 0LL, 0LL}); // peso actual, node , #premiun
+    while(!st.empty()){
+        auto [dis,node,n_premiun] = *st.begin();
+        st.erase(st.begin());
+
+        for(auto [adjN,w,p] : adj[node]){
+            // sin premium
+            if(dis+w < dist[adjN][n_premiun]){
+                if(dist[adjN][n_premiun] != INF){
+                    st.erase({dist[adjN][n_premiun],adjN,n_premiun});
+                }
+
+                dist[adjN][n_premiun]=dis+w;
+                st.insert({dist[adjN][n_premiun],adjN,n_premiun});
+            }
+            // usando un premium
+            if(p != -1 && n_premiun < k){
+                if(dis+p < dist[adjN][n_premiun+1]){
+                    if(dist[adjN][n_premiun+1] != INF){
+                        st.erase({dist[adjN][n_premiun+1],adjN,n_premiun+1});
+                    }
+
+                    dist[adjN][n_premiun+1]=dis+p;
+                    st.insert({dist[adjN][n_premiun+1],adjN,n_premiun+1});
+                }
+            }
+        }
+    }
+
+    long long mn=INF;
+    for(int i=0 ; i<=k ; i++) mn=min(mn,dist[n-1][i]);
+    cout<< mn <<"\n";
+    return 0;
+}
